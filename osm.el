73e8aec3680e056f413e58af2fc43b3bc9167323
Daniel Mendler <mail@daniel-mendler.de>
2022-03-07 18:06:24 +0100

    Pretty pin

--- osm.el
+++ osm.el
@@ -611,10 +611,13 @@ xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink'>
                                   (mapconcat
                                    (pcase-lambda (`(,color ,x . ,y))
                                      (format "
-<g fill='%s' stroke='#000000' transform='translate(%s %s)'>
-<polygon points='0 0 7 -35 -7 -35'/>
-<circle cx='0' cy='-35' r='12'/>
-</g>" color x y))
+<g fill='%s' stroke='#000000' stroke-width='9' transform='translate(%s %s) scale(0.09) translate(-256 -512)'>
+<path d='M256 0C167.641 0 96 71.625 96 160c0 24.75 5.625 48.219 15.672
+69.125C112.234 230.313 256 512 256 512l142.594-279.375
+C409.719 210.844 416 186.156 416 160C416 71.625 344.375
+0 256 0z M256 256c-53.016 0-96-43-96-96s42.984-96 96-96
+c53 0 96 43 96 96S309 256 256 256z'/>
+</g>" color x y)) ;; https://commons.wikimedia.org/wiki/File:Simpleicons_Places_map-marker-1.svg
                                    pins "")
                                   "</svg>"))
             (list :type
