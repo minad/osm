0404b6bc56ff7262f99296e6a40d894177a08d7d
Daniel Mendler <mail@daniel-mendler.de>
2023-01-01 12:10:47 +0100

    Update copyright

--- osm-ol.el
+++ osm-ol.el
@@ -1,6 +1,6 @@
 ;;; osm-ol.el --- Org links for `osm-mode' -*- lexical-binding: t -*-
 
-;; Copyright (C) 2022  Free Software Foundation, Inc.
+;; Copyright (C) 2022-2023 Free Software Foundation, Inc.
 
 ;; This file is part of GNU Emacs.
 
