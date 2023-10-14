1957cf75127471bce0ebe91b5afdf566d5bec033
Daniel Mendler <mail@daniel-mendler.de>
2023-10-14 07:30:00 +0200

    Version 0.14

--- osm.el
+++ osm.el
@@ -5,7 +5,7 @@
 ;; Author: Daniel Mendler <mail@daniel-mendler.de>
 ;; Maintainer: Daniel Mendler <mail@daniel-mendler.de>
 ;; Created: 2022
-;; Version: 0.13
+;; Version: 0.14
 ;; Package-Requires: ((emacs "27.1") (compat "29.1.4.2"))
 ;; Homepage: https://github.com/minad/osm
 ;; Keywords: network, multimedia, hypermedia, mouse
