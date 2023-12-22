2b62c42f152dc952855ac62baa4f80d6c2a63bd4
Daniel Mendler <mail@daniel-mendler.de>
2023-12-22 10:14:24 +0100

    Update commentary

--- osm.el
+++ osm.el
@@ -27,14 +27,15 @@
 
 ;;; Commentary:
 
-;; Osm.el is a tile-based map viewer, with a responsive moveable and
+;; Osm.el is a tile-based map viewer, with a responsive movable and
 ;; zoomable display.  The map can be controlled with the keyboard or with
 ;; the mouse.  The viewer fetches the map tiles in parallel from tile
 ;; servers via the `curl' program.  The package comes with a list of
 ;; multiple preconfigured tile servers.  You can bookmark your favorite
-;; locations using regular Emacs bookmarks or create links from Org
-;; files to locations.  Furthermore the package provides commands to
-;; search for locations by name and to open and display GPX tracks.
+;; locations using regular Emacs bookmarks or create links from Org files
+;; to locations.  Furthermore the package provides commands to measure
+;; distances, search for locations by name and to open and display GPX
+;; tracks.
 
 ;; osm.el requires Emacs 27 and depends on the external `curl' program.
 ;; Emacs must be built with libxml, libjansson, librsvg, libjpeg and
