d14e43bb05a96762eef1b2c7c8c1de51767ed1e9
Daniel Mendler <mail@daniel-mendler.de>
2025-12-19 20:20:53 +0100

    Fix #52

--- osm.el
+++ osm.el
@@ -60,7 +60,7 @@
   :prefix "osm-")
 
 (defcustom osm-curl-options
-  "--user-agent emacs-osm/1.0 --disable --fail --location --silent --max-time 30"
+  " --disable --user-agent emacs-osm/1.0 --fail --location --silent --max-time 30"
   "Curl command line options."
   :type 'string)
 
